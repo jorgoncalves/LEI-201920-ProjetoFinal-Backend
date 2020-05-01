5b26ef36fb38727c9150f9f6f191978e5697442f
Tomás Barreira <tomas.barreira14@outlook.pt>
2020-05-01 18:53:07 +0100

    Update to User_Auth model

--- models/User_Auth.js
+++ models/User_Auth.js
@@ -1,19 +1,34 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const sequelize = require('../util/database');
+const sequelize = require("../util/database");
 
-const User_Auth = sequelize.define('user_auth', {
-  userID: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
+const User_Auth = sequelize.define(
+  "user_auth",
+  {
+    userID: {
+      type: Sequelize.UUID,
+      autoIncrement: false,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.literal("uuid_generate_v4()"),
+    },
+    email: { type: Sequelize.STRING, allowNull: false, unique: true },
+    password: { type: Sequelize.STRING, allowNull: false },
+    is_active: { type: Sequelize.BOOLEAN, allowNull: false },
+    created_on: {
+      type: "TIMESTAMP WITHOUT TIME ZONE",
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_on: {
+      type: "TIMESTAMP WITHOUT TIME ZONE",
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
   },
-  email: { type: Sequelize.STRING, allowNull: false },
-  password: { type: Sequelize.STRING, allowNull: false },
-  is_active: {type: Sequelize.BOOLEAN,
-    allowNull: false,
-  },
-});
+  {
+    timestamps: false,
+  }
+);
 
 module.exports = User_Auth;
