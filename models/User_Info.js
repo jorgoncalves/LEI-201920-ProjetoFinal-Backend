4a3c003af007af1f7a334e0234efcf4613600a37
Tomás Barreira <tomas.barreira14@outlook.pt>
2020-05-01 18:56:10 +0100

    Added new model User_info

---
+++ models/User_Info.js
@@ -0,0 +1,45 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database");
+const User_Auth = require("./User_Auth")
+
+const User_Info = sequelize.define(
+  "user_info",
+  {
+    userID: {
+      type: Sequelize.UUID,
+      autoIncrement: false,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.literal("uuid_generate_v4()"),
+    },
+    name: { type: Sequelize.STRING, allowNull: false },
+    country: { type: Sequelize.STRING, allowNull: false },
+    country_code: { type: Sequelize.STRING, allowNull: true },
+    phone_number: { type: Sequelize.STRING, allowNull: true },
+    user_display: {
+      type: Sequelize.JSON,
+      allowNull: false,
+      defaultValue: "{}",
+    },
+    email: { type: Sequelize.STRING, allowNull: false, unique: true },
+    created_on: {
+      type: "TIMESTAMP WITHOUT TIME ZONE",
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_on: {
+      type: "TIMESTAMP WITHOUT TIME ZONE",
+      allowNull: false,
+      defaultValue: Sequelize.NOW,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+User_Info.hasOne(User_Auth);
+User_Auth.belongsTo(User_Info);
+
+module.exports = User_Info;
